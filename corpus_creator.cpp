d73283722fd9e8dfe482991547a6d7da14ecafe1
Davide Toldo <davide.toldo@stud.tu-darmstadt.de>
2020-08-09 04:26:01 +0200

    Add command line options to output hexdump-style data if just want to monitor and 'log' mode to create corpus file.

--- corpus_creator.cpp
+++ corpus_creator.cpp
@@ -1,11 +1,60 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
 #include <sys/socket.h>
 #include <sys/kern_control.h>
 #include <sys/kern_event.h>
 #include <sys/ioctl.h>
+
+using namespace std;
+
+int ahex2int(char a, char b) {
+    a = (a <= '9') ? a - '0' : (a & 0x7) + 9;
+    b = (b <= '9') ? b - '0' : (b & 0x7) + 9;
+    return (a << 4) + b;
+}
+
+void format(string str) {
+    int string_len = str.size();
+    int line_written_chars = 0;
+
+    for (int i=0; i<string_len; i++) {
+        if ( i%2 ) {
+            printf("%c ", str[i]);
+            line_written_chars += 2;
+        }
+        else {
+            printf("%c", str[i]);
+            line_written_chars += 1;
+        }
+
+        if ( i%8 == 7 ) {
+            printf(" ");
+            line_written_chars += 1;
+        }
+
+        bool need_ascii_now = false;
+        if ( i+1 == string_len && i%32 != 31 ) {
+            need_ascii_now = true;
+            for (int k=0; k<(52-line_written_chars); k++) printf(" ");
+        }
+
+        if ( i%32 == 31 || need_ascii_now ) {
+            for (int j=(i-(i%32))/2; j<((i-31)/2)+16; j++) {
+                if ( j%4 == 0 ) printf("|");
+                int character = (char)ahex2int(str[j*2], str[j*2+1]);
+                printf("%c", character > 32 && character < 127 ? (char)character : '.');
+            }
+            printf("|\n");
+            line_written_chars = 0;
+        }
+    }
+
+    printf("\n\n");
+}
 
 int err(int code) {
     switch (code) {
@@ -23,8 +72,13 @@ int err(int code) {
     exit(-1);
 }
 
-int main(const int argc, char **argv)
-{
+int main(const int argc, char **argv) {
+    if (argc < 2 || (strcmp(argv[1], "-log") && strcmp(argv[1], "-monitor"))) {
+        printf("Usage: corpus_creator [-log] or\n\
+                      [-monitor]\n");
+        return -1;
+    }
+
     struct sockaddr_ctl addr;
     bzero(&addr, sizeof(addr));
     addr.sc_len     = sizeof(addr);
@@ -50,8 +104,13 @@ int main(const int argc, char **argv)
     if (rc) err(3);
     printf("[OK] Connection created. Return: %d\n", rc);
 
-    FILE *fp = fopen("corpus.txt", "w");
-    if (NULL == fp) err(4);
+    // If it's not monitor, but log,
+    // open a file to save the output to.
+    FILE *fp;
+    if (strcmp(argv[1], "-monitor")) {
+        fp = fopen("corpus.txt", "w");
+        if (NULL == fp) err(4);
+    }
 
     while (true) {
         uint8_t buffer[256];
@@ -65,9 +124,21 @@ int main(const int argc, char **argv)
         char output[2*(rc-1) + 1];
         char *ptr = &output[0];
         for (int i=0; i<rc-1; i++) ptr += sprintf(ptr, "%02X", buffer[i]);        
-        fflush(fp);
-        fprintf(fp, "%s\n", output);
+        
+        // Save to file if it's log mode.
+        if (strcmp(argv[1], "-monitor")) {
+            fflush(fp);
+            fprintf(fp, "%s\n", output);
+        }
 
+        // Print raw hex string in any case
         printf("%s\n", output);
+
+        // If it's not log, but monitor,
+        // output in hexdump style
+        if (strcmp(argv[1], "-log")) {
+            string str(output);
+            format(str);
+        }
     }
 }
